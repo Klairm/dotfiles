0a6a2b79308fef8225c9d2d61c9e9b342eb1d334
Ivan Garcia <ivangardis@proton.me>
2025-06-09 00:28:35 +0200

    feat: add more todo keywords for org

--- .emacs.d/lisp/org-config.el
+++ .emacs.d/lisp/org-config.el
@@ -1,7 +1,8 @@
 (setq org-directory "~/MEGA/Notes")
 (setq todo-file (concat org-directory "/todo.org"))
 (setq org-agenda-files (directory-files-recursively org-directory "\\.org$"))
-
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "DROPPED" "DONE")))
 (setq org-capture-templates
       '(("t" "Copy to the TODO list" entry
          (file todo-file)
